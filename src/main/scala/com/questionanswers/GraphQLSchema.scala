6fce2f76b13c7efcfd2d7979d4d8d849d7e327cd
anahcolus <lazymesh@gmail.com>
2018-08-23 11:12:41 +0545

    relation between question and user is defined so that user name can be extracted by using postedBy id of each question

--- src/main/scala/com/questionanswers/GraphQLSchema.scala
+++ src/main/scala/com/questionanswers/GraphQLSchema.scala
@@ -10,6 +10,7 @@ import sangria.ast.StringValue
 import sangria.execution.deferred._
 import sangria.schema.{Field, ObjectType, Schema, _}
 import sangria.streaming.akkaStreams._
+import sangria.macros.derive._
 
 import scala.concurrent.ExecutionContext
 
@@ -31,10 +32,13 @@ object GraphQLSchema {
     }
   )
 
-
   val questionByUserRel = Relation[Question, Int]("questionByUser", q => Seq(q.postedBy))
+  val userByQuestionRel = Relation[User, Int]("userByQuestion", q => Seq(q.userId))
 
-  val usersFetcher = Fetcher((ctx: ApplicationContext, ids: Seq[Int]) => ctx.dao.getUsers(ids))
+  val usersFetcher = Fetcher.rel(
+    (ctx: ApplicationContext, ids: Seq[Int]) => ctx.dao.getUsers(ids),
+    (ctx: ApplicationContext, ids: RelationIds[User]) => ctx.dao.getUsers(ids(userByQuestionRel))
+  )
   val questionsFetcher = Fetcher.rel(
     (ctx: ApplicationContext, texts: Seq[String]) => ctx.dao.getQuestions(texts),
     (ctx: ApplicationContext, ids: RelationIds[Question]) => ctx.dao.getQuestionsByUser(ids(questionByUserRel))
@@ -42,28 +46,20 @@ object GraphQLSchema {
 
   val Resolver = DeferredResolver.fetchers(usersFetcher, questionsFetcher)
 
+  lazy val questionType: ObjectType[Unit, Question] = deriveObjectType[Unit, Question](
+    AddFields(
+      Field("user", OptionType(userType), resolve = c => usersFetcher.deferRelOpt(userByQuestionRel, c.value.postedBy))
+    )
+  )
+
+  lazy val userType: ObjectType[ApplicationContext, User] = deriveObjectType[ApplicationContext, User](
+    AddFields(
+      Field("questions", ListType(questionType), resolve = c => questionsFetcher.deferRelSeq(questionByUserRel, c.value.userId)),
+      Field("lastQuestion", questionType, resolve = c => c.ctx.dao.getLatestQustionByUserId(c.value.userId))
+    )
+  )
 
   def schema(implicit ec: ExecutionContext, mat: Materializer) = {
-
-    val questionType = ObjectType[Unit, Question](
-      "Question",
-      fields[Unit, Question](
-        Field("text", StringType, resolve = _.value.text),
-        Field("answer", StringType, resolve = _.value.answer),
-        Field("postedBy", IntType, resolve = _.value.postedBy),
-        Field("createdAt", GraphQLDateTime, resolve = _.value.createdAt)
-      )
-    )
-
-    val userType = ObjectType[ApplicationContext, User](
-      "User",
-      fields[ApplicationContext, User](
-        Field("userId", IntType, resolve = _.value.userId),
-        Field("name", StringType, resolve = _.value.name),
-        Field("questions", ListType(questionType), resolve = c => questionsFetcher.deferRelSeq(questionByUserRel, c.value.userId)),
-        Field("lastQuestion", questionType, resolve = c => c.ctx.dao.getLatestQustionByUserId(c.value.userId))
-      )
-    )
 
     val query = ObjectType(
       "Query",
